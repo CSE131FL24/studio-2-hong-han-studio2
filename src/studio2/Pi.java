66fe7a08aa368131bc38e4eff9468955fa131c60
junhohong <junhohong@localhost>
2024-09-11 16:10:05 -0500

    Finished Pi

--- src/studio2/Pi.java
+++ src/studio2/Pi.java
@@ -6,7 +6,19 @@ public class Pi {
 		double pi;
 		double x = Math.random();
 		double y = Math.random();
-		double radius = Math.sqrt(y*x+y*y);
+		double radius = Math.sqrt(x*x+y*y);
+		System.out.println(radius);
+		double area=0;
+		int i;
+		for(i=1;i<500000000;i++) {
+			double xtest = Math.random();
+			double ytest = Math.random();
+			if(Math.sqrt(xtest*xtest+ytest*ytest)<radius){
+				area++;
+			}
+		}
+		area = area/i;
+		pi = (4*area)/(radius*radius);
+		System.out.println(pi);
 	}
-
 }
